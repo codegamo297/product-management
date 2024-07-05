f5d55536926274a4188403645f5f359541a5f880
Đỗ Tất Hiếu <dotathieu0729@gmail.com>
2024-07-05 20:05:19 +0700

    Làm trình soạn thảo văn bản

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "/public")));
 
+// New Route to the TinyMCE Node module
+app.use(
+    "/tinymce",
+    express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+
 // Tooltip message (notification)
 app.use(
     session({
