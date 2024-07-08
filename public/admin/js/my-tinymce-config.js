0670916dd58d150f73c0357be2fe7e5eed4e9247
Đỗ Tất Hiếu <dotathieu0729@gmail.com>
2024-07-08 10:08:16 +0700

    Thêm phần upload ảnh ở trình soạn thảo của tinymce

--- public/admin/js/my-tinymce-config.js
+++ public/admin/js/my-tinymce-config.js
@@ -2,4 +2,27 @@ tinymce.init({
     selector: "textarea.text-area-mce",
     plugins: "lists link image table code help wordcount",
     license_key: "gpl",
+    file_picker_callback: (cb, value, meta) => {
+        const input = document.createElement("input");
+        input.setAttribute("type", "file");
+        input.setAttribute("accept", "image/*");
+
+        input.addEventListener("change", (e) => {
+            const file = e.target.files[0];
+
+            const reader = new FileReader();
+            reader.addEventListener("load", () => {
+                const id = "blobid" + new Date().getTime();
+                const blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                const base64 = reader.result.split(",")[1];
+                const blobInfo = blobCache.create(id, file, base64);
+                blobCache.add(blobInfo);
+
+                cb(blobInfo.blobUri(), { title: file.name });
+            });
+            reader.readAsDataURL(file);
+        });
+
+        input.click();
+    },
 });
