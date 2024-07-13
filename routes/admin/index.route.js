a44de1401a93897c103baaf1f0b6ca10ac65235d
Đỗ Tất Hiếu <dotathieu0729@gmail.com>
2024-07-13 15:25:33 +0700

    Làm tính năng đăng nhập

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,6 +5,7 @@ const dustbinRoutes = require("./dustbin.route");
 const productCategoryRoutes = require("./productCategory.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
+const authRoutes = require("./auth.route");
 
 module.exports = (app) => {
     app.use(systemConfig.prefixAdmin + "/dashboard", dashboardRoutes);
@@ -16,4 +17,5 @@ module.exports = (app) => {
     app.use(systemConfig.prefixAdmin + "/dustbin", dustbinRoutes);
     app.use(systemConfig.prefixAdmin + "/roles", roleRoutes);
     app.use(systemConfig.prefixAdmin + "/accounts", accountRoutes);
+    app.use(systemConfig.prefixAdmin + "/auth", authRoutes);
 };
