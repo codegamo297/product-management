3c804b02d90b19bb29c020fbd02474d606499ea3
Đỗ Tất Hiếu <dotathieu0729@gmail.com>
2024-07-15 09:41:19 +0700

    Thêm logs lịch sử thay đổi của nhóm quyền và phân quyền

--- models/role.model.js
+++ models/role.model.js
@@ -3,17 +3,26 @@ const mongoose = require("mongoose");
 // Schema
 const Schema = mongoose.Schema;
 
-const rolesSchema = new Schema(
-    {
-        title: { type: String, required: true },
-        description: String,
-        permissions: { type: Array, default: [] },
-        deleted: { type: Boolean, default: false },
+const rolesSchema = new Schema({
+    title: { type: String, required: true },
+    description: String,
+    permissions: { type: Array, default: [] },
+    deleted: { type: Boolean, default: false },
+    deletedAt: Date,
+    createdBy: {
+        account_id: String,
+        createdAt: { type: Date, default: new Date() },
+    },
+    updatedBy: [
+        {
+            account_id: String,
+            updatedAt: Date,
+        },
+    ],
+    deletedBy: {
+        account_id: String,
         deletedAt: Date,
     },
-    {
-        timestamps: true,
-    }
-);
+});
 
 module.exports = mongoose.model("Roles", rolesSchema, "roles");
