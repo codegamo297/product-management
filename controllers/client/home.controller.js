d4cf7e8bb649254fa62a32153da5d0beffbcdc27
Đỗ Tất Hiếu <dotathieu0729@gmail.com>
2024-07-17 09:31:25 +0700

    Làm trang chủ bên client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,4 +1,29 @@
+const Products = require("../../models/product.model");
+const productHelper = require("../../helpers/product");
+
 // [GET] /
 module.exports.index = async (req, res) => {
-    res.render("client/pages/home/index", { pageTitle: "Trang chủ" });
+    // Lấy ra sản phẩm nổi bật
+    const productsFeatured = await Products.find({
+        deleted: false,
+        featured: "1",
+        status: "active",
+    }).limit(6);
+    const newProductsFeatured =
+        productHelper.priceNewProducts(productsFeatured);
+
+    // Lấy ra sản phẩm nổi bật
+    const productsNew = await Products.find({
+        deleted: false,
+        status: "active",
+    })
+        .sort({ position: "desc" })
+        .limit(6);
+    const newProductsNew = productHelper.priceNewProducts(productsNew);
+
+    res.render("client/pages/home/index", {
+        pageTitle: "Trang chủ",
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew,
+    });
 };
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+const productHelper = require("../../helpers/product");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -7,14 +8,8 @@ module.exports.index = async (req, res) => {
         deleted: false,
     }).sort({ position: "desc" });
 
-    const newProducts = products.map((item) => {
-        item.priceNew = (
-            item.price *
-            (1 - item.discountPercentage / 100)
-        ).toFixed(2);
+    const newProducts = productHelper.priceNewProducts(products);
 
-        return item;
-    });
     res.render("client/pages/products/index", {
         pageTitle: "Trang sản phẩm",
         products: newProducts,
