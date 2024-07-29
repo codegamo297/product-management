bde0015281c02dec8e911149bcab212725aad4de
Đỗ Tất Hiếu <dotathieu0729@gmail.com>
2024-07-29 16:25:51 +0700

    Sửa lại header bên client

--- middlewares/client/user.middleware.js
+++ middlewares/client/user.middleware.js
@@ -1,4 +1,8 @@
 const User = require("../../models/user.model");
+const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+
+const productHelper = require("../../helpers/product");
 
 module.exports.requireUser = async (req, res, next) => {
     const tokenUser = req.cookies.tokenUser;
@@ -12,6 +16,34 @@ module.exports.requireUser = async (req, res, next) => {
 
         if (user) {
             res.locals.user = user;
+
+            const cart = await Cart.findById(req.cookies.cartId);
+
+            if (cart.products.length > 0) {
+                for (const product of cart.products) {
+                    const productId = product.product_id;
+                    const productInfo = await Product.findOne({
+                        _id: productId,
+                        deleted: false,
+                    }).select("title thumbnail price discountPercentage");
+
+                    productInfo.priceNew =
+                        productHelper.priceNewProduct(productInfo);
+                    product.productInfo = productInfo;
+                    product.totalPrice = (
+                        parseFloat(product.productInfo.priceNew) *
+                        product.quantity
+                    ).toFixed(2);
+                }
+            }
+
+            cart.totalPrice = cart.products
+                .reduce((total, item) => {
+                    return total + parseFloat(item.totalPrice);
+                }, 0)
+                .toFixed(2);
+
+            res.locals.cart = cart;
         }
     }
 
--- public/js/script.js
+++ public/js/script.js
@@ -15,3 +15,36 @@ if (showAlert) {
 }
 
 // End Show alert
+
+// Top act search
+// const topActSearch = document.querySelector(".top-act__search");
+
+// if (topActSearch) {
+//     const input = topActSearch.querySelector("input");
+//     const separate = topActSearch.querySelector(".top-act__separate");
+
+//     if (input.value) {
+//         input.style.display = "block";
+//         separate.style.opacity = "1";
+//     }
+
+//     input.addEventListener("focus", (e) => {
+//         input.style.display = "block";
+//         separate.style.opacity = "1";
+//     });
+
+//     input.addEventListener("blur", (e) => {
+//         if (!e.target.value) {
+//             input.style.removeProperty("display");
+//             separate.style.removeProperty("opacity");
+//         }
+//     });
+
+//     input.addEventListener("change", (e) => {
+//         if (e.target.value) {
+//             input.style.display = "block";
+//         } else {
+//             input.style.removeProperty("display");
+//         }
+//     });
+// }
